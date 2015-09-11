47dd544b5d9252a7d102cab5146d3284fa9d6c89
Eric Kuhn <erickuhn19@gmail.com>
2015-09-11 17:30:38 -0400

    finished version of FunFacts

---
+++ FunFacts/Scratchpad.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Swift Recap
+
+var someVariable = "aVariable"
+let someConstant = 20
+
+someVariable = "anotherString"
+
+var fruitsArray: [String] = ["Apples", "Oranges"]
+fruitsArray.append("banna")
+fruitsArray
+
+// Optionals
+
+var optionalString: String? = "hello"
+optionalString!
+optionalString = nil
+
+//Random Number Generation
+
+var randomNumber = Int(arc4random_uniform(10))
+
+
+// UIColor
+
+var redColor = UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0)
+
+let colorsArray
+
+
+
+
+
+
+
+
+
+
+
+
